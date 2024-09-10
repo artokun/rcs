ed3f785c022c131d3847d4df6371f9e241dcb9df
Arthur R Longbottom <art.longbottom.jr@gmail.com>
2024-09-09 20:24:11 -0700

    target tracking

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,34 @@
 use std::f32::consts::PI;
 
-use avian3d::prelude::*;
+#[cfg(debug_assertions)]
+use avian3d::debug_render::PhysicsDebugPlugin;
+use avian3d::{
+    prelude::{
+        AngularDamping, Collider, ColliderDensity, ExternalAngularImpulse, ExternalImpulse,
+        Gravity, Inertia, LinearVelocity, Mass, RigidBody, SleepingDisabled,
+    },
+    PhysicsPlugins,
+};
+#[cfg(debug_assertions)]
 use bevy::diagnostic::FrameTimeDiagnosticsPlugin;
 use bevy::{
     asset::LoadState,
+    color::palettes::css::GOLD,
     core_pipeline::Skybox,
     prelude::*,
     render::render_resource::{TextureViewDescriptor, TextureViewDimension},
 };
+#[cfg(debug_assertions)]
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
 use bevy_panorbit_camera::{PanOrbitCamera, PanOrbitCameraPlugin};
 
 #[derive(Component)]
-struct Ship;
+pub struct Ship;
+#[derive(Component)]
+pub struct Target;
+
+#[derive(Component)]
+pub struct AttitudeText;
 
 #[derive(Resource)]
 struct Cubemap {
@@ -33,10 +49,11 @@ fn main() {
             }),
             PanOrbitCameraPlugin,
             PhysicsPlugins::default(),
-            #[cfg(not(feature = "production"))]
+            #[cfg(debug_assertions)]
             PhysicsDebugPlugin::default(),
+            #[cfg(debug_assertions)]
             FrameTimeDiagnosticsPlugin,
-            #[cfg(not(feature = "production"))]
+            #[cfg(debug_assertions)]
             WorldInspectorPlugin::new(),
         ))
         .insert_resource(Gravity(Vec3::ZERO))
@@ -45,9 +62,19 @@ fn main() {
             color: Color::WHITE,
             brightness: 10.0,
         })
-        .add_systems(Startup, (setup_camera, setup_light, setup_ship))
-        .add_systems(Update, asset_loaded)
-        .add_systems(FixedPostUpdate, cam_follow)
+        .add_systems(
+            Startup,
+            (
+                setup_camera,
+                setup_light,
+                setup_ship,
+                setup_target,
+                setup_ui,
+            ),
+        )
+        .add_systems(PreUpdate, asset_loaded)
+        .add_systems(FixedUpdate, update_controls)
+        .add_systems(FixedPostUpdate, (cam_follow, update_ui))
         .run();
 }
 
@@ -62,14 +89,38 @@ fn setup_ship(
         Name::new("Ship"),
         RigidBody::Dynamic,
         Collider::cuboid(dimensions.x, dimensions.y, dimensions.z),
+        ExternalImpulse::new(Vec3::new(0.0, 0.0, 0.0)),
+        ExternalAngularImpulse::new(Vec3::new(0.0, 0.0, 0.0)),
+        AngularDamping(0.0),
         PbrBundle {
             mesh: meshes.add(Cuboid::new(dimensions.x, dimensions.y, dimensions.z)),
             material: materials.add(Color::srgb(0.5, 0.5, 0.5)),
             ..default()
         },
-        AngularVelocity(Vec3::ZERO),
-        LinearVelocity(Vec3::ZERO),
         SleepingDisabled,
+    ));
+}
+
+fn setup_target(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn((
+        Target,
+        Name::new("Target"),
+        RigidBody::Dynamic,
+        Collider::cuboid(1.0, 1.0, 1.0),
+        PbrBundle {
+            mesh: meshes.add(Cuboid::new(1.0, 1.0, 1.0)),
+            material: materials.add(StandardMaterial {
+                base_color: Color::srgb(0.0, 0.5, 0.0),
+                emissive: LinearRgba::rgb(5.0, 15.0, 13.0),
+                ..default()
+            }),
+            transform: Transform::from_xyz(5.0, 5.0, -20.0),
+            ..default()
+        },
     ));
 }
 
@@ -147,5 +198,150 @@ fn cam_follow(mut pan_orbit_q: Query<&mut PanOrbitCamera>, ship_q: Query<&Transf
             pan_orbit.target_focus = ship_tfm.translation;
             pan_orbit.force_update = true;
         }
+    }
+}
+
+pub fn setup_ui(mut commands: Commands) {
+    commands.spawn((
+        TextBundle::from_section(
+            "Loading",
+            TextStyle {
+                font_size: 20.0,
+                color: GOLD.into(),
+                ..default()
+            },
+        )
+        .with_style(Style {
+            position_type: PositionType::Absolute,
+            top: Val::Px(5.0),
+            right: Val::Px(15.0),
+            ..default()
+        }),
+        AttitudeText,
+    ));
+}
+
+pub fn update_ui(
+    ship_q: Query<(&Transform, &LinearVelocity), With<Ship>>,
+    target_q: Query<(&Transform, &LinearVelocity), With<Target>>,
+    mut text_query: Query<&mut Text, With<AttitudeText>>,
+) {
+    if let (Ok((ship_transform, ship_linvel)), Ok((target_transform, target_linvel))) =
+        (ship_q.get_single(), target_q.get_single())
+    {
+        if let Ok(mut text) = text_query.get_single_mut() {
+            let relative_pos = target_transform.translation - ship_transform.translation;
+            let range = relative_pos.length();
+
+            // Calculate azimuth (horizontal angle)
+            let local_relative_pos = ship_transform.rotation.inverse() * relative_pos;
+            let mut az = local_relative_pos
+                .x
+                .atan2(-local_relative_pos.z)
+                .to_degrees();
+            az = if az > 180.0 {
+                az - 360.0
+            } else if az < -180.0 {
+                az + 360.0
+            } else {
+                az
+            };
+
+            // Calculate elevation (vertical angle)
+            let el = (-local_relative_pos.y)
+                .atan2((-local_relative_pos.z).hypot(local_relative_pos.x))
+                .to_degrees();
+
+            let v_rel = target_linvel.0 - ship_linvel.0;
+            let local_v_rel = ship_transform.rotation.inverse() * v_rel;
+
+            // Calculate cross-track velocities in ship's local frame
+            let v_crs_y = local_v_rel.y;
+            let v_crs_x = local_v_rel.x;
+
+            // Improved ETA calculation
+            let closing_velocity = -v_rel.dot(relative_pos.normalize());
+            let eta = if closing_velocity.abs() > 0.1 {
+                range / closing_velocity
+            } else {
+                f32::INFINITY
+            };
+
+            text.sections[0].value = format!(
+                "RNG: {:.2} m\nAZ: {:.2}°\nEL: {:.2}°\nVREL: {:.2} m/s\nVCRS_Y: {:.2} m/s\nVCRS_X: {:.2} m/s\nETA: {:.2} s",
+                range,
+                az,
+                el,
+                v_rel.length(),
+                v_crs_y,
+                v_crs_x,
+                eta
+            );
+        }
+    }
+}
+
+fn update_controls(
+    mut ship_q: Query<
+        (
+            &Transform,
+            &mut ExternalImpulse,
+            &mut ExternalAngularImpulse,
+            &mut AngularDamping,
+        ),
+        With<Ship>,
+    >,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+) {
+    let strength = 0.01;
+    let mut force_vec = Vec3::ZERO;
+    let mut torque_vec = Vec3::ZERO;
+    if let Ok((transform, mut lin_impulse, mut ang_impulse, mut damping)) = ship_q.get_single_mut()
+    {
+        if keyboard_input.pressed(KeyCode::KeyW) {
+            force_vec.z -= strength;
+        }
+        if keyboard_input.pressed(KeyCode::KeyS) {
+            force_vec.z += strength;
+        }
+        if keyboard_input.pressed(KeyCode::KeyA) {
+            force_vec.x -= strength;
+        }
+        if keyboard_input.pressed(KeyCode::KeyD) {
+            force_vec.x += strength;
+        }
+        if keyboard_input.pressed(KeyCode::Space) {
+            force_vec.y += strength;
+        }
+        if keyboard_input.pressed(KeyCode::ControlLeft) {
+            force_vec.y -= strength;
+        }
+        if keyboard_input.pressed(KeyCode::KeyE) {
+            torque_vec.z -= strength;
+        }
+        if keyboard_input.pressed(KeyCode::KeyQ) {
+            torque_vec.z += strength;
+        }
+        if keyboard_input.pressed(KeyCode::KeyR) {
+            damping.0 = 5.0;
+        } else if keyboard_input.just_released(KeyCode::KeyR) {
+            damping.0 = 0.0;
+        }
+        if keyboard_input.pressed(KeyCode::ArrowUp) {
+            torque_vec.x -= strength;
+        }
+        if keyboard_input.pressed(KeyCode::ArrowDown) {
+            torque_vec.x += strength;
+        }
+        if keyboard_input.pressed(KeyCode::ArrowLeft) {
+            torque_vec.y += strength;
+        }
+        if keyboard_input.pressed(KeyCode::ArrowRight) {
+            torque_vec.y -= strength;
+        }
+        let rotated_force = transform.rotation * force_vec;
+        let rotated_torque = transform.rotation * torque_vec;
+        lin_impulse.apply_impulse(rotated_force.clamp_length_max(strength));
+        ang_impulse.apply_impulse(rotated_torque.clamp_length_max(strength));
     }
 }
